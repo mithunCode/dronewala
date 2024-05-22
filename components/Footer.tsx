eb655d688bd0639567a27e399acaa40f79bb1218
Mithun <mithunnaikwork@gmail.com>
2024-05-22 15:42:03 +0530

    Layout Implemented

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="w-full h-[10rem] bg-black text-gray-300 text-center pt-10">
-      Footer with Contact Details , Social Media Links , Address .
+      Footer with Contact Details , Social Media Links , Address ,etc.
     </div>
   );
 };
